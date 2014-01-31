394e2cc16de2cac28c1203c27464e3a876705d61
BryanJenks <bryan.jenks@red-matter.com>
2014-01-31 16:12:48 -0500

    Automated documentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,12 +5,25 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON('package.json'),
         nodeunit: {
             all: ['test/*_test.js']
+        },
+        apidoc: {
+            myapp: {
+                src: "",
+                dest: "public/doc",
+                includeFilters: [ ".*\.js$" ],
+                excludeFilters: [ "node_modules/" ],
+                debug: false,
+                marked: {
+                    gfm: true
+                }
+            }
         }
     });
 
     // load plugins
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-apidoc');
 
     // Default task(s).
-    grunt.registerTask('default', ['nodeunit']);
+    grunt.registerTask('default', ['nodeunit', 'apidoc']);
 }
